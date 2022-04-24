359210cc5620cf3646fe234edd8c67772dfd9e35
Angel <angelflores992@gmail.com>
2022-04-24 10:13:29 -0500

    add

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -30,36 +29,3 @@ app.listen(PORT, () => {
 });
 
 // comment too
-=======
-import express, { Application, Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import morgan from "morgan";
-
-import { main } from "./db/index.js";
-import NoteRouter from "./Routes/NoteRouter.js";
-
-dotenv.config();
-
-const app: Application = express();
-const { PORT } = process.env;
-
-main().catch((err) => {
-  console.log(err);
-});
-
-app.use(morgan("tiny"));
-app.use(express.json());
-app.use(cors());
-app.use("/todo", NoteRouter);
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Success" });
-});
-
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
-
-// comment too
->>>>>>> 059ff79cdb848701daf678698e6e4e5e9648f142
